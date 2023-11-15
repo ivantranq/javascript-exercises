54981f25b9c9366c1e28a35bbeae66b6360b23f8
Ivan <ivan.tranquilan@outlook.com>
2023-11-15 16:37:30 +1030

    06 all tests passed

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,13 @@
-const leapYears = function() {
-
+const leapYears = function(inputYear) {
+  if (inputYear % 100 === 0) {
+    // Century year
+    if (inputYear % 400 === 0) {
+      return true;
+    }
+  } else if (inputYear % 4 === 0) {
+    return true
+  }
+  return false
 };
 
 // Do not edit below this line
