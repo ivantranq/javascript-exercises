7feb085b0231389014236cf239a0e2214c17d520
Ivan <ivan.tranquilan@outlook.com>
2023-11-15 16:30:00 +1030

    05 all tests passed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,21 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+  
+  if (typeof(num1) !== 'number' || 
+  typeof(num2) !== 'number' ||
+  num1 < 0 ||
+  num2 < 0)
+  {
+    return 'ERROR';
+  };
+
+  let startNum = Math.min(num1, num2);
+  let endNum = Math.max(num1,num2);
+  let sum = 0;
+
+  for (let i = startNum; i <= endNum; i++) {
+    sum += i;
+  }
+  return sum;
 
 };
 
