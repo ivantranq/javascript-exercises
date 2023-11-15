7613ac9513f0807207ae9a40e4ce39277ac22006
Ivan <ivan.tranquilan@outlook.com>
2023-11-15 16:47:33 +1030

    07 all tests passed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degreesFarenheit) {
+  let degreesCelsius = (degreesFarenheit - 32)*(5/9);
+  degreesCelsius = Math.round(degreesCelsius*10)/10;
+  return degreesCelsius
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degreesCelsius) {
+  let degreesFarenheit = degreesCelsius * (9/5) + 32;
+  degreesFarenheit = Math.round(degreesFarenheit*10)/10;
+  return degreesFarenheit
 };
 
 // Do not edit below this line
