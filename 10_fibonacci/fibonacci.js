2080a117800dcba05a5bad58769a6decdf493fcf
Ivan <ivan.tranquilan@outlook.com>
2023-11-18 17:01:50 +1030

    10 all tests passed

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(nthNum) {
+  if (typeof(nthNum) === 'string') nthNum = parseInt(nthNum);
+  if (nthNum < 1) return 'OOPS';
+  const fibSequence = [1,1];
+  let l = fibSequence.length;
+  while (l < nthNum) {
+    fibSequence.push(fibSequence[l-1] + fibSequence[l-2]);
+    l = fibSequence.length;
+  } 
+  
+  return fibSequence[l-1];
 };
 
 // Do not edit below this line
