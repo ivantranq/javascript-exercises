5f9a43e2807a3f1646324345f8b75a75dae2f5ca
Ivan <ivan.tranquilan@outlook.com>
2023-11-18 16:43:25 +1030

    all tests passed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
-
+const palindromes = function (inputString) {
+  // let arrayString = inputString.split('');
+  // let puncRemoved = arrayString.filter(char => !!char.match(/[.,:!?]/)).join('');
+  let normalisedInput = inputString.split('')
+                                    .filter(char => !char.match(/[., :!?]/))
+                                    .join('')
+                                    .toLowerCase();
+  let reversedString = normalisedInput.split('')
+                                      .reverse()
+                                      .join('');
+  console.log(normalisedInput);
+  console.log(reversedString);
+  return reversedString === normalisedInput;
 };
 
 // Do not edit below this line
