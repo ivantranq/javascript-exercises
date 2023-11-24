ba0fd95b77d77c523a36397b3eaae6bc81b637ec
Ivan <ivan.tranquilan@outlook.com>
2023-11-24 23:24:59 +1030

    12_findTheOldest all tests passed

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+  let ages = people.map(person => {
+    if (!person.yearOfDeath) {
+      let currentDate = new Date(); 
+      let currentYear = currentDate. getFullYear();
+      return currentYear - person.yearOfBirth
+    }
+    
+    return person.yearOfDeath - person.yearOfBirth});
+  console.log(ages);
+  let index = ages.indexOf(Math.max(...ages));
+  return people[index];
+  
 };
 
 // Do not edit below this line
